3178b2242049bb96c152a88af5426e5b0b71a94e
maryageorgescu <mg3200@nyu.edu>
2019-02-11 08:19:16 -0500

    Added 2 scripts. One is a player controller, one is to shift the lights in the space.

---
+++ Some Horrible Thing/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Use: Place on player controller.
+//Intent: to be able to look around.
+public class MouseLook : MonoBehaviour
+{
+    public enum RotationAxes
+    {
+        MouseXAndY= 0,
+        MouseX= 1,
+        MouseY= 2
+    }
+
+    public RotationAxes axes = RotationAxes.MouseXAndY;
+
+    public float sensitivityHor = 9.0f;
+    public float sensitivityVert = 9.0f;
+
+    public float minimumVert = -45.0f;
+    public float maximumVert = 45.0f;
+
+    private float _rotationX = 0;
+
+    private void Start()
+    {
+        Rigidbody body = GetComponent<Rigidbody>(); 
+        // we are disabling physics in order to make sure all movement is only controlled by the mouse
+        if (body != null)
+            body.freezeRotation = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (axes == RotationAxes.MouseX){
+           transform.Rotate(0, Input.GetAxis("Mouse X")* sensitivityHor, 0); //horizontal rotation 
+        }
+        else if (axes == RotationAxes.MouseY)
+        {
+            _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVert;
+            _rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert);
+
+            float rotationY = transform.localEulerAngles.y;
+            
+            transform.localEulerAngles = new Vector3(_rotationX,rotationY, 0);//vertical rotation 
+        }
+        else
+        {
+            _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVert;
+            _rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert);
+
+            float delta = Input.GetAxis("Mouse X") * sensitivityHor;
+            float rotationY = transform.localEulerAngles.y + delta;
+            transform.localEulerAngles=new Vector3(_rotationX, rotationY, 0);//both horizontal and vertical
+        }
+    }
+}
---
+++ Some Horrible Thing/Assets/Scripts/Movement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+//Use: Place on lights.
+//Intent: To create an uneasy feeling. To make some lights shift in and out of the space or warp the space somehow
+
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 3.0f;
+    public float maxZ = 13.0f;
+    public float minZ = - 13.0f;
+
+    private int _direction = 1;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0,0,_direction *speed *Time.deltaTime);
+
+        bool bounced = false;
+        if (transform.position.z > maxZ || transform.position.z < minZ)
+        {
+            _direction = -_direction;
+            bounced = true;
+        }
+
+        if (bounced)
+        {
+            transform.Translate(0,0,_direction*speed* Time.deltaTime);
+        }
+
+    }
+}
